d2909ee00630fe9359d648ce26a001acff21da29
unknown <diesol42@gmail.com>
2019-07-08 19:50:55 -0700

    finished assigment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,160 +1,161 @@
+var card = $("#quiz-area");
 
-
-// display a timer
-var area = $("#quiz");
-
-var myQuestions = [
+// Questions
+var questions = [
     {
     question: "Our atmosphere is approximately what percent oxygen?",
-    choices: [  "39%",
-                "72",
-                "21%"],
-    answer: "21%"
+    answers: [      "39%",
+                    "72",
+                    "21%"],
+    correctAnswer:  "21%"
 
     },
 
 {
     question: "When facing mount Rushmore, what Presidents head is furthest on the right?",
-    choices: [   "George Washington",
-                 "Abraham Lincoln",
-                 "Teddy Roosevelt"],
-    answer: "Abraham Lincoln"
+    answers: [      "George Washington",
+                    "Abraham Lincoln",
+                    "Teddy Roosevelt"],
+    correctAnswer:  "Abraham Lincoln"
     },
 
 {
     question: "What is the world's most profitable company?",
-    choices: [  "Amazon",
-                "Apple",
-                "Google"],
-    answer: "Apple"
+    answers: [      "Amazon",
+                    "Apple",
+                    "Google"],
+    correctAnswer:  "Apple"
     },
 
 {
     question: "Common problem as a child, what is nocturnal enuresis?",
-    choices: [  "Sleepwalk",
-                "Bed Wetting",
-                "Insomnia"],
-    answer: "Bed Wetting"
+    answers: [      "Sleepwalk",
+                    "Bed Wetting",
+                    "Insomnia"],
+    correctAnswer:  "Bed Wetting"
     },
 
 {
     question: "John Goodman originally voiced which animated character?",
-    choices: [  "Yellow M&M",
-                "Buzz Lightyear",
-                "Chester Cheetah"],
-    answer: "Yellow M&M"
+    answers: [      "Yellow M&M",
+                    "Buzz Lightyear",
+                    "Chester Cheetah"],
+    correctAnswer:  "Yellow M&M"
     },
         
 {
     question: "Which of these products will help remove crayon stains from clothing?",
-    choices: [  "Pinesol",
-                "Windez",
-                "WD-40"],
-    answer: "WD-40"
+    answers: [      "Pinesol",
+                    "Windex",
+                    "WD-40"],
+    correctAnswer:  "WD-40"
     },
         
 {
     question: "Where is the Sea of Tranquility located?",
-    choices: [  "The Moon",
-                "Brazil",
-                "Australia"],
-    answer: "The Moon"
+    answers: [      "The Moon",
+                    "Brazil",
+                    "Australia"],
+    correctAnswer:  "The Moon"
     },
         
 {
     question: "Which chess piece can only move diagonally?",
-    choices: [  "Rook",
-                "Knight",
-                "Bishop"],
-    answer: "Bishop"
+    answers: [      "Rook",
+                    "Knight",
+                    "Bishop"],
+    correctAnswer:  "Bishop"
     },
         
 {
     question: "What was the first planet discovered by using a telescope in 1781?",
-    choices: [  "Mars",
-                "Uranus",
-                "Jupiter"],
-    answer: "Uranus"
+    answers: [      "Mars",
+                    "Uranus",
+                    "Jupiter"],
+    correctAnswer:  "Uranus"
     },
         
 {
     question: "Why doesn't a duck's quack echo?",
-    choices: [  "Unique Vocal Chords",
-                "Hollow Beaks",
-                "Nobody Knows"],
-    answer: "Nobody Knows"
+    answers: [      "Unique Vocal Chords",
+                    "Hollow Beaks",
+                    "Nobody Knows"],
+    correctAnswer:  "Nobody Knows"
     },
         
 {
     question: "What is the Milky Way?",
-    choices: [  "Chocolate Bar",
-                "A form of stars",
-                "Our Galaxy"],
-    answer: "Our Galaxy"
+    answers: [      "Chocolate Bar",
+                    "A form of stars",
+                    "Our Galaxy"],
+    correctAnswer:  "Our Galaxy"
     },
 ];
 
-var counter
+var timer;
+
 var game = {
-    correct: 0,
-    incorrect: 0,
-    timer: 40,
+  correct: 0,
+  incorrect: 0,
+  counter: 60,
 
-    countdown: function() {
-        game.timer--;
-        $("#timer-number").html(game.timer);
-        if (game.timer === 0) {
-            console.log("Time Up!");
-            game.done();
-        }
-    },
-
-    start: function() {
-        counter = setInterval(game.countdown, 1000);
-
-        $("#main-timer").prepend(
-            "<h2>Time Remaining: <span id='counter-number'>40</span> Seconds</h2>"
-        );
-
-        $("#start").remove();
-
-        for (var i = 0; i < myQuestions.length; i++) {
-            area.append("<h2>" + myQuestions[i].question + "</h2>");
-            for (var j = 0; j < myQuestions[i].choices.length; j++) {
-                area.append("<input type='radio' name='question-" + i +
-                "' value='" + myQuestions[i].choices[j] + "''>" + myQuestions[i].choices[j]);
-            }
-        }
-
-        area.append("<button id ='done'> Done</button>");
-    },
-
-    done: function() {
-        var inputs = area.children("input:checked");
-        for (var i = 0; i < inputs.length; i++) {
-            if ($(inputs[i]).val () === myQuestions[i].answer) {
-                game.correct++;
-            }   else{
-                game.incorrect++;
-            }
-        }
-        this.result();
-    },
-
-    result: function() {
-        clearInterval(counter);
-        $("#main-timer h2").remove();
-
-        area.html("<h2> Done!</h2>");
-        area.append("<h3>Correct Answers: " + this.correct + "</h3>");
-        area.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
     }
+  },
+
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#sub-wrapper").prepend(
+      "<h2>Time Remaining: <span id='counter-number'>60</span> Seconds</h2>"
+    );
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      card.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        card.append("<input type='radio' name='question-" + i +
+          "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
+
+    card.append("<button id='done'>Done</button>");
+  },
+
+  done: function() {
+    var inputs = card.children("input:checked");
+    for (var i = 0; i < inputs.length; i++) {
+      if ($(inputs[i]).val() === questions[i].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    }
+    this.result();
+  },
+
+  result: function() {
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    card.html("<h2>All Done!</h2>");
+    card.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+  }
 };
 
-$(docuemnt).on("click", "#start", function() {
-    game.start();
+
+$(document).on("click", "#start", function() {
+  game.start();
 });
 
 $(document).on("click", "#done", function() {
-    game.done();
+  game.done();
 });
